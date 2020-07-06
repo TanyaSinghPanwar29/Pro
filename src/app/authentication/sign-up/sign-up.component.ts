e719483b1f25ca824b21fec33e4f1f2a2ae5ac39
rhlmalik17 <rhlmalik17@gmail.com>
2020-07-06 20:57:13 +0530

    Sign Up Screen

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToSignIn = () => {
+    this.router.navigateByUrl('login')
   }
 
 }
